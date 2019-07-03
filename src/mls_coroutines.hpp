6ec9da65bd16eb970ee734b2ea162927c49ce090
Tyler Drake <tyler.drake@protonmail.com>
2019-07-03 19:09:03 -0400

    Retab source files

--- src/mls_coroutines.hpp
+++ src/mls_coroutines.hpp
@@ -2,15 +2,15 @@
 #pragma once
 
 #define MLS_CO_BEGIN(return_type, ...)                                \
-	[mls_co_context = 0, __VA_ARGS__]() mutable -> return_type {      \
-		constexpr static auto kCoAnchor = (__COUNTER__, __COUNTER__); \
-		static_assert(kCoAnchor % 2, "kCoAnchor must be odd!");       \
-		switch (mls_co_context) {                                     \
-	    case 0:
+    [mls_co_context = 0, __VA_ARGS__]() mutable -> return_type {      \
+        constexpr static auto kCoAnchor = (__COUNTER__, __COUNTER__); \
+        static_assert(kCoAnchor % 2, "kCoAnchor must be odd!");       \
+        switch (mls_co_context) {                                     \
+        case 0:
 
 #define MLS_CO_END }}
 
 #define MLS_CO_YIELD(result)            \
     mls_co_context = (__COUNTER__ / 2); \
-	return result;                      \
+    return result;                      \
     case (__COUNTER__ - 1) / 2:
--- src/test.cpp
+++ src/test.cpp
@@ -4,20 +4,20 @@
 
 auto count_up_from(int start)
 {
-	return MLS_CO_BEGIN(int, start)
-	{
-		while (true) {
-			MLS_CO_YIELD(start);
-			start += 1;
-		}
-	} MLS_CO_END;
+    return MLS_CO_BEGIN(int, start)
+    {
+        while (true) {
+            MLS_CO_YIELD(start);
+            start += 1;
+        }
+    } MLS_CO_END;
 }
 
 int main(int argc, char** argv)
 {
-	auto next = count_up_from(42);
-	for (int i = 0; i < 10; i++) {
-		std::cout << next() << std::endl;
-	}
-	return 0;
+    auto next = count_up_from(42);
+    for (int i = 0; i < 10; i++) {
+        std::cout << next() << std::endl;
+    }
+    return 0;
 }
